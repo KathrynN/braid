f7b8bebaaa2864b385435ac130a7e7b3ebf1d7e2
Kathryn <kathryn.newbould@anaplan.com>
2018-06-23 09:10:54 +0100

    Reformatted files, adding non-url user id as addable content

--- src/VideoColumn.js
+++ src/VideoColumn.js
@@ -9,8 +9,7 @@ import {
   generateJSONRequestForUserRecentUploads
 } from "./utilities";
 import { List } from "react-virtualized";
-import FlexView from 'react-flexview';
-
+import FlexView from "react-flexview";
 
 export default class VideoColumn extends Component {
   videos_to_request = 50;
@@ -24,46 +23,50 @@ export default class VideoColumn extends Component {
 
   render() {
     let videos;
-    if (this.state.content.length > 0){
+    if (this.state.content.length > 0) {
       let content = this.state.content;
-      if(retrieve_from_local_storage("hide_watched") === true) {
+      if (retrieve_from_local_storage("hide_watched") === true) {
         content = content.filter(e => {
           return !is_video_watched(this.get_video_id(e));
-        })
+        });
       }
-      const contentVideoComponents = this.convert_list_of_ids_to_thumbnails(content);
-      videos = <List
-            width={500}
-            height={window.innerHeight - 205}
-            rowCount={content.length}
-            rowHeight={150}
-            rowRenderer={({ key, index, isScrolling, isVisible, style }) =>
-              this.rowRenderer({
-                key,
-                index,
-                isScrolling,
-                isVisible,
-                style,
-                contentVideoComponents
-              })
-            }
-          />
+      const contentVideoComponents = this.convert_list_of_ids_to_thumbnails(
+        content
+      );
+      videos = (
+        <List
+          width={500}
+          height={window.innerHeight - 205}
+          rowCount={content.length}
+          rowHeight={150}
+          rowRenderer={({ key, index, isScrolling, isVisible, style }) =>
+            this.rowRenderer({
+              key,
+              index,
+              isScrolling,
+              isVisible,
+              style,
+              contentVideoComponents
+            })
+          }
+        />
+      );
     } else {
-      videos = <div></div>
+      videos = <div />;
     }
 
     return (
-        <FlexView column>
-          <h2>{this.find_column_title()}</h2>
-          <a
-            onClick={() => {
-              this.props.remove_subscription(this.props.content_id);
-            }}
-          >
-            x
-          </a>
-          {videos}
-        </FlexView>
+      <FlexView column>
+        <h2>{this.find_column_title()}</h2>
+        <a
+          onClick={() => {
+            this.props.remove_subscription(this.props.content_id);
+          }}
+        >
+          x
+        </a>
+        {videos}
+      </FlexView>
     );
   }
 
@@ -75,7 +78,7 @@ export default class VideoColumn extends Component {
     style, // Style object to be applied to row (to position it)
     contentVideoComponents
   }) {
-    if((contentVideoComponents.length - index) < 5 && this.can_request_more) {
+    if (contentVideoComponents.length - index < 5 && this.can_request_more) {
       this.getMoreVideos();
     }
     return (
@@ -85,50 +88,65 @@ export default class VideoColumn extends Component {
     );
   }
 
-  getMoreVideos(){
+  getMoreVideos() {
     this.can_request_more = false;
     const token = this.state.nextPageToken;
     this.getListOfContent(this.props.source_info, token);
   }
 
   getListOfContent(source_info, source_token_id) {
-    const source_token_request=(source_token_id!==undefined)
-        ? "&pageToken="+source_token_id
-        : ""
+    const source_token_request =
+      source_token_id !== undefined ? "&pageToken=" + source_token_id : "";
     if (source_info.content_type === "channel") {
       getDataFromYoutube(
-        generateJSONRequestForChannel(this.props.content_id, this.videos_to_request)+source_token_request
+        generateJSONRequestForChannel(
+          this.props.content_id,
+          this.videos_to_request
+        ) + source_token_request
       ).then(data => {
         let content_of_column = this.state.content.slice().concat(data.items);
         this.setState({
-           content: content_of_column,
-           nextPageToken: data.nextPageToken
-         })
-         this.can_request_more=true;
+          content: content_of_column,
+          nextPageToken: data.nextPageToken
+        });
+        this.can_request_more = true;
       });
     } else if (source_info.content_type === "user") {
-      generateJSONRequestForUserRecentUploads(this.props.content_id, this.videos_to_request).then(
-        json_request =>
-          getDataFromYoutube(json_request+source_token_request, this.videos_to_request).then(data => {
-            let content_of_column = this.state.content.slice().concat(data.items);
+      generateJSONRequestForUserRecentUploads(
+        this.props.content_id,
+        this.videos_to_request
+      ).then(json_request => {
+        if (json_request === "user has no videos") {
+          this.props.remove_subscription(this.props.content_id);
+        } else {
+          getDataFromYoutube(
+            json_request + source_token_request,
+            this.videos_to_request
+          ).then(data => {
+            let content_of_column = this.state.content
+              .slice()
+              .concat(data.items);
             this.setState({
-               content: content_of_column,
-               nextPageToken: data.nextPageToken
-             })
-             this.can_request_more=true;
-           }
-          )
-      );
+              content: content_of_column,
+              nextPageToken: data.nextPageToken
+            });
+            this.can_request_more = true;
+          });
+        }
+      });
     } else if (source_info.content_type === "playlist") {
       getDataFromYoutube(
-        generateJSONRequestForPlaylist(this.props.content_id, this.videos_to_request)
+        generateJSONRequestForPlaylist(
+          this.props.content_id,
+          this.videos_to_request
+        )
       ).then(data => {
         let content_of_column = this.state.content.slice().concat(data.items);
         this.setState({
-           content: content_of_column,
-           nextPageToken: data.nextPageToken
-         })
-         this.can_request_more=true;
+          content: content_of_column,
+          nextPageToken: data.nextPageToken
+        });
+        this.can_request_more = true;
       });
     }
   }
@@ -167,23 +185,15 @@ export default class VideoColumn extends Component {
   }
 
   take_video_object_return_thumbnail(video_object) {
-    return (
-        <VideoThumbnail
-          video_object={video_object}
-        />
-    );
+    return <VideoThumbnail video_object={video_object} />;
   }
 
-  convert_list_of_ids_to_thumbnails(
-    playlist_items
-  ) {
+  convert_list_of_ids_to_thumbnails(playlist_items) {
     if (playlist_items[0] === undefined) {
       return;
     }
     return playlist_items.map(json_for_me => {
-      let video_object = this.map_json_response_to_video_object(
-        json_for_me
-      );
+      let video_object = this.map_json_response_to_video_object(json_for_me);
       return this.take_video_object_return_thumbnail(video_object);
     });
   }
--- src/YoutubeBigPlayer.js
+++ src/YoutubeBigPlayer.js
@@ -1,62 +1,60 @@
-import React, { Component } from 'react';
-import YouTube from 'react-youtube';
+import React, { Component } from "react";
+import YouTube from "react-youtube";
 
 export default class YoutubeBigPlayer extends Component {
-
-  constructor(props){
+  constructor(props) {
     super(props);
     this.escFunction = this.escFunction.bind(this);
   }
 
-  opts =
-  {
+  opts = {
     playerVars: {
       autoplay: 1,
       rel: 0
     }
-  }
+  };
 
   escFunction(event) {
-    if(event.keyCode === 27) {
-      this.props.closeNav()
+    if (event.keyCode === 27) {
+      this.props.closeNav();
     }
   }
 
-  componentDidMount(){
+  componentDidMount() {
     document.addEventListener("keydown", this.escFunction, false);
   }
-  componentWillUnmount(){
+  componentWillUnmount() {
     document.removeEventListener("keydown", this.escFunction, false);
   }
 
   render() {
-    if(this.props.video_object !== undefined && this.props.video_object.video_id !== undefined){
+    if (
+      this.props.video_object !== undefined &&
+      this.props.video_object.video_id !== undefined
+    ) {
       const video_id = this.props.video_object.video_id;
-      console.log("Video_object", this.props.video_object);
-      return <div className="overlay"
-        id="overlay-player">
-        <a className="closebtn" onClick={() => this.props.closeNav()}>&times;</a>
-        <YouTube
-          className="overlay-content centered"
-          id="big_youtube_player"
-          videoId={video_id}
-          key={video_id}
-          opts={this.opts}
-          allowFullScreen="allowFullScreen"
-          onStateChange={(event) => {
-               if(event.data === 0) {
-                 this.props.video_object.add_to_watched(video_id)
-                 this.props.closeNav()
-               }
-          }
-        }
-
-        />
-      </div>
+      return (
+        <div className="overlay" id="overlay-player">
+          <a className="closebtn" onClick={() => this.props.closeNav()}>
+            &times;
+          </a>
+          <YouTube
+            className="overlay-content centered"
+            id="big_youtube_player"
+            videoId={video_id}
+            key={video_id}
+            opts={this.opts}
+            allowFullScreen="allowFullScreen"
+            onStateChange={event => {
+              if (event.data === 0) {
+                this.props.video_object.add_to_watched(video_id);
+                this.props.closeNav();
+              }
+            }}
+          />
+        </div>
+      );
     }
-    return <div></div>
+    return <div />;
   }
-
-
-
 }
